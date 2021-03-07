170bcc69bcaa0ac440857926f167f597f5ae4c1b
serge-mur <serge-mur@yandex.ru>
2021-03-07 13:54:53 +0300

    add slideout

--- js/script.js
+++ js/script.js
@@ -1,6 +1,39 @@
+// https://slideout.js.org/
+var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70,
+    'side': 'right',
+    'touch': false
+});
+
+function close(eve) {
+    eve.preventDefault();
+    slideout.close();
+}
+slideout
+    .on('beforeopen', function() {
+        this.panel.classList.add('panel-open');
+    })
+    .on('open', function() {
+        this.panel.addEventListener('click', close);
+    })
+    .on('beforeclose', function() {
+        this.panel.classList.remove('panel-open');
+        this.panel.removeEventListener('click', close);
+    });
+
+document.querySelector('.mobile-menu-toggler').addEventListener('click', function() {
+    slideout.toggle();
+});
+
+
+// https://kenwheeler.github.io/slick/
 $('.slider').slick({
     infinite: true,
 });
+
 $('.main-product-slider').slick({
     infinite: true,
 });
