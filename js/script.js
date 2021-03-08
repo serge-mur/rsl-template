1975d623aa154aa10eb72fd396b3eb3be16cd0cb
serge-mur <serge-mur@yandex.ru>
2021-03-08 17:49:47 +0300

    test slideout

--- js/script.js
+++ js/script.js
@@ -28,6 +28,31 @@ document.querySelector('.mobile-menu-toggler').addEventListener('click', functio
     slideout.toggle();
 });
 
+var fixed = document.querySelector('.top-block-mobile');
+
+slideout.on('translate', function(translated) {
+    fixed.style.transform = 'translateX(' + translated + 'px)';
+});
+
+slideout.on('beforeopen', function() {
+    fixed.style.transition = 'transform 300ms ease';
+    fixed.style.transform = 'translateX(-256px)';
+});
+
+slideout.on('beforeclose', function() {
+    fixed.style.transition = 'transform 300ms ease';
+    fixed.style.transform = 'translateX(0px)';
+});
+
+slideout.on('open', function() {
+    fixed.style.transition = '';
+});
+
+slideout.on('close', function() {
+    fixed.style.transition = '';
+});
+
+
 
 // https://kenwheeler.github.io/slick/
 $('.slider').slick({
