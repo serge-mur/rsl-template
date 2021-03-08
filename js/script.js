020556045743169725cb8285b4f4ccaa3e45fb3e
serge-mur <serge-mur@yandex.ru>
2021-03-08 18:38:59 +0300

    test js offcanvas

--- js/script.js
+++ js/script.js
@@ -1,55 +1,10 @@
-// https://slideout.js.org/
-var slideout = new Slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('menu'),
-    'padding': 256,
-    'tolerance': 70,
-    'side': 'right',
-    'touch': false
-});
-
-function close(eve) {
-    eve.preventDefault();
-    slideout.close();
-}
-slideout
-    .on('beforeopen', function() {
-        this.panel.classList.add('panel-open');
-    })
-    .on('open', function() {
-        this.panel.addEventListener('click', close);
-    })
-    .on('beforeclose', function() {
-        this.panel.classList.remove('panel-open');
-        this.panel.removeEventListener('click', close);
+$(function() {
+    // https://github.com/vmitsaras/js-offcanvas
+    $('#right').offcanvas({
+        modifiers: "right,overlay",
+        triggerButton: '.js-offcanvas-trigger-right',
+        resize: true
     });
-
-document.querySelector('.mobile-menu-toggler').addEventListener('click', function() {
-    slideout.toggle();
-});
-
-var fixed = document.querySelector('.top-block-mobile');
-
-slideout.on('translate', function(translated) {
-    fixed.style.transform = 'translateX(' + translated + 'px)';
-});
-
-slideout.on('beforeopen', function() {
-    fixed.style.transition = 'transform 300ms ease';
-    fixed.style.transform = 'translateX(-256px)';
-});
-
-slideout.on('beforeclose', function() {
-    fixed.style.transition = 'transform 300ms ease';
-    fixed.style.transform = 'translateX(0px)';
-});
-
-slideout.on('open', function() {
-    fixed.style.transition = '';
-});
-
-slideout.on('close', function() {
-    fixed.style.transition = '';
 });
 
 
